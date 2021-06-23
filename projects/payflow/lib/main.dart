89586877ad91dbfcc4b7fb16768e7d5e47cbe476
Gabriel Rodrigues <33073778+gabrielroodriz@users.noreply.github.com>
2021-06-23 06:35:27 -0300

    fix: Adding initial widget in test file

--- projects/payflow/lib/main.dart
+++ projects/payflow/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/modules/login/login_page.dart';
-import 'package:payflow/modules/splash/splash_page.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 
 void main() {
